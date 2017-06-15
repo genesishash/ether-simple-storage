12cf9ec57a51e64ea5b8009d5b6a07098e9f15dc
tosadvisor <taky@taky.com>
2017-06-15 16:07:29 -0400

    auto

--- src/contract.sol
+++ src/contract.sol
@@ -3,18 +3,35 @@ pragma solidity ^0.4.8;
 contract SimpleStorage
 {
   string public current_value;
-  address public last_changed_by;
 
-  function SimpleStorage(string value)
-  {
+  address public last_changed_by;
+  address owner;
+
+  // const
+  function SimpleStorage(string value){
+    owner = msg.sender;
+
     current_value = value;
     last_changed_by = msg.sender;
   }
 
-  function change(string value)
-  {
+  // change current value
+  function change_value(string value){
     current_value = value;
     last_changed_by = msg.sender;
+    return true;
+  }
+
+  // return current value
+  function current_value() constant returns (string){
+    return current_value;
+  }
+
+  // recover funds
+  function kill(){
+    if(msg.sender==owner){
+      selfdestruct(owner);
+    }
   }
 }
 
