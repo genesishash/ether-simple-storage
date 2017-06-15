724262383fc716f8cd4efa8a4998a5341d1ec518
tosadvisor <taky@taky.com>
2017-06-15 16:46:11 -0400

    auto

--- src/contract.sol
+++ src/contract.sol
@@ -7,26 +7,31 @@ contract SimpleStorage
   address public last_changed_by;
   address owner;
 
-  // const
+  event value_changed(string value, address addr);
+  event contract_created(string name);
+
+  //
   function SimpleStorage(string value){
     owner = msg.sender;
 
     current_value = value;
     last_changed_by = msg.sender;
+
+    contract_created(name);
+    value_changed(current_value,last_changed_by);
   }
 
-  // change current value
   function change_value(string value){
     current_value = value;
     last_changed_by = msg.sender;
+    value_changed(current_value,last_changed_by);
   }
 
-  // return current value
   function current_value() constant returns (string){
     return current_value;
   }
 
-  // recover funds
+  //
   function kill(){
     if(msg.sender==owner){
       selfdestruct(owner);
